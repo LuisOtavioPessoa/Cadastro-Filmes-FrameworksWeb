fea88ad8932c294f0d618bb8d1f2cf1fe29004e3
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2024-11-16 21:30:52 -0300

    Front-end + Botões
    Adicionando os Botões de cadastrar e listar

--- React/aulareact/src/App.tsx
+++ React/aulareact/src/App.tsx
@@ -1,13 +1,19 @@
 import { FormContatos } from "./components/FormContatos";
 import Header from "./components/Header";
 import { ListaContatos } from "./components/ListaContatos";
+import ButtonComponent from "./components/Button"; 
+
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
       <main>
+        <br />
         <FormContatos/>
+        <br />
+        <ButtonComponent />
+        <br /> <br />
         <ListaContatos />
       </main>
     </div>
---
+++ React/aulareact/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
+
+function ButtonComponent() {
+  return (
+    <Container>
+      <Button href="#">Cadastrar</Button>
+      <Button href="#">Listar</Button>
+
+    </Container>
+  );
+}
+
+export default ButtonComponent;

d9c0e73ebfb72c954615ac0b1b9298ce3d4e6bd5
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2024-11-16 21:52:36 -0300

    Front-end + botão cadastrar funcional
    A tela é iniciada apenas com a lista. Quando clica no botão cadastrar o formulário é exibido e ao clicar no botão listar o formulário desaparece.

--- React/aulareact/src/App.tsx
+++ React/aulareact/src/App.tsx
@@ -1,18 +1,26 @@
+import React, { useState } from "react";
 import { FormContatos } from "./components/FormContatos";
 import Header from "./components/Header";
 import { ListaContatos } from "./components/ListaContatos";
 import ButtonComponent from "./components/Button"; 
-
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
 
 const App: React.FC = () => {
+  const [mostrarForm, setMostrarForm] = useState(false);
+
   return (
     <div>
       <Header />
       <main>
         <br />
-        <FormContatos/>
+        {mostrarForm && <FormContatos/>}
         <br />
-        <ButtonComponent />
+        <Container>
+        <Button onClick={() => setMostrarForm(true)}>Cadastrar</Button>
+
+        <Button onClick={() => setMostrarForm(false)}>Listar</Button>
+        </Container>
         <br /> <br />
         <ListaContatos />
       </main>
